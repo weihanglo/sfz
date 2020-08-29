a198e77639fa08a3e13b83036e579bc8aa5931c7
Weihang Lo <me@weihanglo.tw>
2020-08-29 13:07:12 +0800

    refactor: fix content_encoding

--- src/http/content_encoding.rs
+++ src/http/content_encoding.rs
@@ -12,57 +12,115 @@ use std::io::{self, BufReader};
 use brotli;
 use flate2::read::{DeflateEncoder, GzEncoder};
 use flate2::Compression;
-use hyper::header::{q, AcceptEncoding, Encoding, QualityItem};
-use hyper::Request;
+use hyper::header::HeaderValue;
 
-/// Sorting encodings according to the rank:
+const IDENTITY: &'static str = "identity";
+const DEFLATE: &'static str = "deflate";
+const GZIP: &'static str = "gzip";
+const BR: &'static str = "br";
+
+/// Inner helper type to store quality values.
 ///
-/// 1. Brotili
-/// 2. Gzip
-/// 3. Deflate
+/// - 0: content enconding
+/// - 1: weight from 0 to 1000
+#[derive(Debug, PartialEq)]
+struct QualityValue<'a>(&'a str, u32);
+
+/// Inner helper type for comparsion by intrinsic enum variant order.
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+enum Encoding {
+    Identity,
+    Deflate,
+    Gzip,
+    Brotli,
+}
+
+impl From<&str> for Encoding {
+    fn from(s: &str) -> Self {
+        match s {
+            DEFLATE => Self::Deflate,
+            GZIP => Self::Gzip,
+            BR => Self::Brotli,
+            _ => Self::Identity,
+        }
+    }
+}
+
+/// This match expression is necessary to return a `&'static str`.
+fn encoding_to_static_str<'a>(encoding: &'a str) -> &'static str {
+    match encoding {
+        DEFLATE => DEFLATE,
+        GZIP => GZIP,
+        BR => BR,
+        _ => IDENTITY,
+    }
+}
+
+/// Sorting encodings according to the weight of quality values and then the
+/// intrinsic rank of `Encoding` enum varaint.
 ///
 /// The function only accecpt Brotli, Gzip and Deflate encodings, passing other
 /// encodings in may lead to a unexpected result.
-fn sort_encoding(a: &&QualityItem<Encoding>, b: &&QualityItem<Encoding>) -> Ordering {
-    a.quality
-        .cmp(&b.quality)
-        .then_with(|| match (&a.item, &b.item) {
-            (&Encoding::Brotli, _) => Ordering::Greater,
-            (_, &Encoding::Brotli) => Ordering::Less,
-            (&Encoding::Gzip, &Encoding::Deflate) => Ordering::Greater,
-            (&Encoding::Deflate, &Encoding::Gzip) => Ordering::Less,
-            _ => Ordering::Equal,
-        })
+fn sort_encoding(a: &QualityValue, b: &QualityValue) -> Ordering {
+    a.1.cmp(&b.1)
+        .then_with(|| Encoding::from(a.0).cmp(&Encoding::from(b.0)))
+}
+
+/// According to RFC7231, a [Quality Values][1] is defined as follow grammar:
+///
+/// ```text
+/// weight = OWS ";" OWS "q=" qvalue
+/// qvalue = ( "0" [ "." 0*3DIGIT ] )
+///        / ( "1" [ "." 0*3("0") ] )
+/// ```
+///
+/// Note that:
+///
+/// - Quality value of 0 means unacceptable.
+/// - The weight ranges from 0 to 1 in real number with three digit at most.
+/// - Weight defaults to 1 if not present.
+/// - We define unrecognized qvalue as zero.
+///
+/// [1]: https://tools.ietf.org/html/rfc7231#section-5.3.1
+fn parse_qvalue(q: &str) -> Option<QualityValue> {
+    let mut iter = q.trim().split_terminator(';').take(2);
+    let content = iter.next().map(str::trim_end)?;
+    let weight = match iter.next() {
+        Some(s) => s
+            .trim_start()
+            .trim_start_matches("q=")
+            .parse::<f32>()
+            .ok()
+            .map(|num| (num * 1000.0) as u32)
+            .filter(|v| *v <= 1000)
+            .unwrap_or_default(),
+        None => 1000,
+    };
+    Some(QualityValue(content, weight))
 }
 
 /// Get prior encoding from `Accept-Encoding` header field.
 ///
-/// To get the priority of accept encodings, compare quality values first.
-/// If one's quality values equals to the other's, sort by actual encodings.
-pub fn get_prior_encoding(req: &Request) -> Encoding {
-    req.headers()
-        .get::<AcceptEncoding>()
+/// Note that:
+///
+/// - Only accept `br` / `gzip` / `deflate`
+/// - Highest non-zero qvalue is preferred.
+pub fn get_prior_encoding<'a>(accept_encoding: &'a HeaderValue) -> &'static str {
+    accept_encoding
+        .to_str()
+        .ok()
         .and_then(|accept_encoding| {
-            let mut vec = vec![];
-            let AcceptEncoding(ref encodings) = *accept_encoding;
-            let zero = q(0); // Zero means unacceptable.
-            for encoding in encodings {
-                if encoding.quality <= zero {
-                    continue;
-                }
-                match encoding.item {
-                    Encoding::Brotli | Encoding::Gzip | Encoding::Deflate => {
-                        vec.push(encoding);
-                    }
-                    _ => (),
-                }
-            }
+            let mut quality_values = accept_encoding
+                .split(',')
+                .filter_map(parse_qvalue)
+                .collect::<Vec<_>>();
             // Sort by quality value, than by encoding type.
-            vec.sort_unstable_by(sort_encoding);
-            vec.pop() // Pop the last (largest).
+            quality_values.sort_unstable_by(sort_encoding);
+            // Get the last encoding (highest priority).
+            quality_values.last().map(|q| encoding_to_static_str(q.0))
         })
-        .map(|encoding| encoding.item.to_owned())
-        .unwrap_or(Encoding::Identity) // Default using identity encoding.
+        // Default using identity encoding, which means no content encoding.
+        .unwrap_or(IDENTITY)
 }
 
 /// Compress data.
@@ -70,39 +128,63 @@ pub fn get_prior_encoding(req: &Request) -> Encoding {
 /// # Parameters
 ///
 /// * `data` - Data to be compressed.
-/// * `encoding` - Only support `Bortli`, `Gzip` and `Deflate`.
-pub fn compress(data: &[u8], encoding: &Encoding) -> io::Result<Vec<u8>> {
+/// * `encoding` - Only support `br`, `gzip`, `deflate` and `identity`.
+pub fn compress(data: &[u8], encoding: &str) -> io::Result<Vec<u8>> {
     use std::io::prelude::*;
     let mut buf = Vec::new();
-    match *encoding {
-        Encoding::Brotli => {
+    match encoding {
+        BR => {
             BufReader::new(brotli::CompressorReader::new(data, 4096, 6, 20))
                 .read_to_end(&mut buf)?;
-            Ok(buf)
         }
-        Encoding::Gzip => {
+        GZIP => {
             BufReader::new(GzEncoder::new(data, Compression::default())).read_to_end(&mut buf)?;
-            Ok(buf)
         }
-        Encoding::Deflate => {
+        DEFLATE => {
             BufReader::new(DeflateEncoder::new(data, Compression::default()))
                 .read_to_end(&mut buf)?;
-            Ok(buf)
         }
-        _ => Err(io::Error::new(io::ErrorKind::Other, "Unsupported Encoding")),
+        _ => return Err(io::Error::new(io::ErrorKind::Other, "Unsupported Encoding")),
+    };
+    Ok(buf)
+}
+
+#[cfg(test)]
+mod t_parse_qvalue {
+    use super::*;
+
+    #[test]
+    fn parse_successfully() {
+        let cases = vec![
+            (Some(QualityValue(BR, 1000)), "br;q=1"),
+            (Some(QualityValue(BR, 0)), "br;q=0"),
+            (Some(QualityValue(BR, 1000)), "br;q=1.000"),
+            (Some(QualityValue(BR, 0)), "br;q=0.000"),
+            (Some(QualityValue(BR, 1000)), "br"),
+            (Some(QualityValue(BR, 1000)), "br;"),
+            (Some(QualityValue(BR, 0)), "br;1234asd"),
+            (Some(QualityValue(BR, 500)), "       br    ;   q=0.5    "),
+            (Some(QualityValue("*", 1000)), "*"),
+            (Some(QualityValue("*", 300)), "*;q=0.3"),
+            (Some(QualityValue("q=123", 1000)), "q=123"),
+            (None, ""),
+        ];
+        for case in cases {
+            let res = parse_qvalue(case.1);
+            assert_eq!(res, case.0, "failed on case: {:?}", case);
+        }
     }
 }
 
 #[cfg(test)]
 mod t_sort {
     use super::*;
-    use hyper::header::{q, qitem};
 
     #[test]
     fn same_qualities() {
-        let brotli = &&qitem(Encoding::Brotli);
-        let gzip = &&qitem(Encoding::Gzip);
-        let deflate = &&qitem(Encoding::Deflate);
+        let brotli = &QualityValue(BR, 1000);
+        let gzip = &QualityValue(GZIP, 1000);
+        let deflate = &QualityValue(DEFLATE, 1000);
         assert_eq!(sort_encoding(brotli, gzip), Ordering::Greater);
         assert_eq!(sort_encoding(brotli, deflate), Ordering::Greater);
         assert_eq!(sort_encoding(gzip, deflate), Ordering::Greater);
@@ -112,62 +194,51 @@ mod t_sort {
 
     #[test]
     fn second_item_with_greater_quality() {
-        let a = QualityItem {
-            item: Encoding::Brotli,
-            quality: q(500),
-        };
-        let b = qitem(Encoding::Gzip);
-        assert_eq!(sort_encoding(&&a, &&b), Ordering::Less);
+        let a = &QualityValue(BR, 500);
+        let b = &QualityValue(DEFLATE, 1000);
+        assert_eq!(sort_encoding(a, b), Ordering::Less);
     }
 }
 
 #[cfg(test)]
 mod t_prior {
     use super::*;
-    use hyper::header::{q, qitem};
-    use hyper::Method;
-
-    #[test]
-    fn no_accept_encoding_header() {
-        let req = Request::new(Method::Get, "localhost".parse().unwrap());
-        assert_eq!(get_prior_encoding(&req), Encoding::Identity);
-    }
+    use hyper::header::HeaderValue;
 
     #[test]
     fn with_unsupported_encoding() {
-        let mut req = Request::new(Method::Get, "localhost".parse().unwrap());
         // Empty encoding
-        let accept_encoding = AcceptEncoding(vec![]);
-        req.headers_mut().set(accept_encoding);
-        assert_eq!(get_prior_encoding(&req), Encoding::Identity);
+        let accept_encoding = HeaderValue::from_static("");
+        let encoding = get_prior_encoding(&accept_encoding);
+        assert_eq!(encoding, IDENTITY);
 
         // Deprecated encoding.
-        let accept_encoding = AcceptEncoding(vec![qitem(Encoding::Compress)]);
-        req.headers_mut().set(accept_encoding);
-        assert_eq!(get_prior_encoding(&req), Encoding::Identity);
+        let accept_encoding = HeaderValue::from_static("compress");
+        let encoding = get_prior_encoding(&accept_encoding);
+        assert_eq!(encoding, IDENTITY);
     }
 
     #[test]
-    fn pick_brotli() {
-        let mut req = Request::new(Method::Get, "localhost".parse().unwrap());
-        let accept_encoding = AcceptEncoding(vec![
-            qitem(Encoding::Deflate),
-            qitem(Encoding::Brotli),
-            qitem(Encoding::Gzip),
-        ]);
-        req.headers_mut().set(accept_encoding);
-        assert_eq!(get_prior_encoding(&req), Encoding::Brotli);
+    fn pick_highest_priority() {
+        let cases = vec![
+            (BR, "br,gzip,deflate"),
+            (BR, "gzip,br,deflate"),
+            (BR, "deflate,gzip,br"),
+            (BR, "br;q=0.8,gzip;q=0.5,deflate;q=0.2"),
+            (GZIP, "br;q=0.5,gzip,deflate;q=0.8"),
+        ];
+        for case in cases {
+            let accept_encoding = HeaderValue::from_static(case.1);
+            let encoding = get_prior_encoding(&accept_encoding);
+            assert_eq!(encoding, case.0, "failed on case: {:?}", case);
+        }
     }
 
     #[test]
     fn filter_out_zero_quality() {
-        let mut req = Request::new(Method::Get, "localhost".parse().unwrap());
-        let accept_encoding = AcceptEncoding(vec![QualityItem {
-            item: Encoding::Brotli,
-            quality: q(0),
-        }]);
-        req.headers_mut().set(accept_encoding);
-        assert_eq!(get_prior_encoding(&req), Encoding::Identity);
+        let accept_encoding = HeaderValue::from_static("brotli;q=0,gzip;q=0,deflate");
+        let encoding = get_prior_encoding(&accept_encoding);
+        assert_eq!(encoding, DEFLATE);
     }
 }
 
@@ -177,7 +248,7 @@ mod t_compress {
 
     #[test]
     fn failed() {
-        let error = compress(b"hello", &Encoding::Identity).unwrap_err();
+        let error = compress(b"hello", "unrecognized").unwrap_err();
         assert_eq!(error.kind(), io::ErrorKind::Other);
     }
 }
