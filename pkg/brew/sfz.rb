4c2976ecc3c4e72ea374e6e80eb6a37670e5debc
Weihang Lo <me@weihanglo.tw>
2020-11-08 08:17:03 +0800

    v0.3.0 homebrew formula [ci skip]

--- pkg/brew/sfz.rb
+++ pkg/brew/sfz.rb
@@ -1,15 +1,15 @@
 class Sfz < Formula
-  version '0.2.1'
+  version '0.3.0'
   desc "A simple static file serving command-line tool."
   homepage "https://github.com/weihanglo/sfz"
   head "https://github.com/weihanglo/sfz.git"
 
   if OS.mac?
       url "https://github.com/weihanglo/sfz/releases/download/v#{version}/sfz-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "0a7980657efb80e477a8f994b0eb54b9011a19657bff73080c3d95b64d1f0e4f"
+      sha256 "baf2ec817b186668a0d8df21f15da09dc80fcfd9810f197c1814be9681cd6ac9"
   elsif OS.linux?
       url "https://github.com/weihanglo/sfz/releases/download/v#{version}/sfz-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "bf36c49b02578b241862a97bb9da92f7ad984f8935f80ee8baf947d1eafba110"
+      sha256 "dadf2d8d56c1c5b5b66e255bac718912f80b269598f997b127cba76e9abcd81e"
   end
 
   def install
