4b4970a9fc118f5ce7405bebd30838cb12ad367d
Weihang Lo <me@weihanglo.tw>
2021-04-19 15:09:25 +0800

    v0.5.0 homebrew formula [ci skip]

--- pkg/brew/sfz.rb
+++ pkg/brew/sfz.rb
@@ -1,15 +1,15 @@
 class Sfz < Formula
-  version '0.4.0'
+  version '0.5.0'
   desc "A simple static file serving command-line tool."
   homepage "https://github.com/weihanglo/sfz"
   head "https://github.com/weihanglo/sfz.git"
 
   if OS.mac?
-      url "https://github.com/weihanglo/sfz/releases/download/v#{version}/sfz-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "ce00eec5bd917dab6a464fc517fa2bad1d2ce3bd8038ce756dc5547ddc4174b2"
+    url "https://github.com/weihanglo/sfz/releases/download/v#{version}/sfz-v#{version}-x86_64-apple-darwin.tar.gz"
+    sha256 "e09c7a493000e8b74428019c01d6a1888e8747147275a8f792ea4a09bc952813"
   elsif OS.linux?
-      url "https://github.com/weihanglo/sfz/releases/download/v#{version}/sfz-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "db964e4762145efa871a6909d5c7f4ad5fbe6659090ce157beff91aca679a72c"
+    url "https://github.com/weihanglo/sfz/releases/download/v#{version}/sfz-v#{version}-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "eaba9a468ce2b8b241b745af4967780baa32f0fbb842892e17d7660c3c2408f5"
   end
 
   def install
