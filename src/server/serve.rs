5a1ed986a977aeba3d59cd76f45750ceba1f437a
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2022-09-07 22:22:14 +0800

    refactor: implement should_compress

--- src/server/serve.rs
+++ src/server/serve.rs
@@ -32,9 +32,9 @@ use serde::Serialize;
 use crate::cli::Args;
 use crate::extensions::{MimeExt, PathExt, SystemTimeExt};
 use crate::http::conditional_requests::{is_fresh, is_precondition_failed};
-use crate::http::content_encoding::{compress, encoding_to_static_str, get_prior_encoding};
+use crate::http::content_encoding::{compress, get_prior_encoding, should_compress};
 use crate::http::range_requests::{is_range_fresh, is_satisfiable_range};
-use crate::http::{BR, DEFLATE, GZIP};
+
 use crate::server::send::{send_dir, send_dir_as_zip, send_file, send_file_with_range};
 use crate::server::{res, Request, Response};
 use crate::BoxResult;
@@ -408,24 +408,19 @@ impl InnerService {
         let mime_type = InnerService::guess_path_mime(&path, action);
 
         let body = if self.can_compress(res.status(), &mime_type) {
-            let encoding = req
-                .headers()
-                .get(hyper::header::ACCEPT_ENCODING)
-                .map(get_prior_encoding)
-                .unwrap_or_default();
-            let enc = encoding_to_static_str(encoding);
-            match enc {
-                BR | DEFLATE | GZIP => {
-                    let body = Body::wrap_stream(
-                        body.map_err(|_e| io::Error::from(io::ErrorKind::InvalidData))
-                            .map(move |b| match b {
-                                Ok(b) => compress(&b, enc),
-                                Err(e) => Err(e),
-                            }),
-                    );
+            if let Some(encoding) = req.headers().get(hyper::header::ACCEPT_ENCODING) {
+                let content_encoding = get_prior_encoding(encoding);
+                if should_compress(content_encoding) {
+                    let stream = body
+                        .map_err(|_e| io::Error::from(io::ErrorKind::InvalidData))
+                        .map(|b| match b {
+                            Ok(b) => compress(&b, content_encoding),
+                            Err(e) => Err(e),
+                        });
+                    let body = Body::wrap_stream(stream);
                     res.headers_mut().insert(
                         hyper::header::CONTENT_ENCODING,
-                        hyper::header::HeaderValue::from_static(encoding),
+                        hyper::header::HeaderValue::from_static(content_encoding),
                     );
                     // Representation varies, so responds with a `Vary` header.
                     res.headers_mut().insert(
@@ -433,9 +428,11 @@ impl InnerService {
                         hyper::header::HeaderValue::from_name(hyper::header::ACCEPT_ENCODING),
                     );
                     body
+                } else {
+                    body
                 }
-                // No Accept-Encoding would not be compress.
-                _ => body,
+            } else {
+                body
             }
         } else {
             // Set Content-Length only when body is not compressed,
